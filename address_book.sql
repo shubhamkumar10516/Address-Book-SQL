1928c925c61cdae043663d4b8915026f8fb6c2b3
shubham <shubhamkumar10516@gmail.com>
2020-10-28 01:50:48 +0530

    [Shubham]ADDED: Creating table command added

--- address_book.sql
+++ address_book.sql
@@ -1,2 +1,15 @@
 CREATE DATABASE address_book_service; #uc1 creating database
 
+CREATE TABLE address_Book (                                #uc2 creating table
+                           firstName varchar(15) not null,
+                           lastName varchar(15),
+                           address varchar(15),
+                           city varchar(15) not null,
+                           state varchar(15) not null,
+                           zip int(6) not null,
+                           phoneNumber bigint(10) not null,
+                           email varchar(30),
+                           PRIMARY KEY(email));
+
+DESCRIBE address_Book;
+
